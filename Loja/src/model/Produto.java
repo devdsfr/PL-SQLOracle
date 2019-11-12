727599f331b6735c291792f19cd77fdfaa37efba
thiago <thiagonospk@gmail.com>
2019-11-12 18:38:52 -0300

    Fiz a classe de conexão, classe produto e classe intem. algumas classe foi criada mas não estar completa.
    Signed-off-by: thiago <thiagonospk@gmail.com>

---
+++ Loja/src/model/Produto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author thiago
+ */
+public class Produto {
+    private String NomeProduto;
+    private int IdProduto;
+    private String descricao;
+    private double valor;
+    private 
+}

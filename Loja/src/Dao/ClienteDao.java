7ce82bee03b48cee9a7158a7628aded65c232706
Daniel <devdsfr@gmail.com>
2019-11-18 21:47:54 -0300

    atualizar
    Signed-off-by: Daniel <devdsfr@gmail.com>

--- Loja/src/Dao/ClienteDao.java
+++ Loja/src/Dao/ClienteDao.java
@@ -5,10 +5,28 @@
  */
 package Dao;
 
+import model.Cliente;
+import connection.ConnectionFactory;
+
 /**
  *
  * @author thiago
  */
 public class ClienteDao {
+    public int incluirDao(Cliente cliente){
+        ConnectionFactory c = new ConnectionFactory();
+        
+        try {
+            c.conectar();
+            String sql = "INSERT INTO TBL(NOME) VALUES (" + cliente.getNome_ou_razao_social() + ");";
+            return c.insertSQL(sql);
+        } catch (Exception e) {
+            System.err.println(e);
+            return 0;
+        } finally{
+            c.fecharConexao();
+        }
+        
+    }
     
 }
